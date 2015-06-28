48ab33576b2385d3763cd00a6e6063ee7b582dce
Jiang Long <jianglong@startimes.com.cn>
2015-06-28 21:35:48 +0800

    some modification

--- src/test/java/tk/jeromefromcn/collector/AntDependenciesCollectorTest.java
+++ src/test/java/tk/jeromefromcn/collector/AntDependenciesCollectorTest.java
@@ -2,6 +2,7 @@ package tk.jeromefromcn.collector;
 
 import static org.junit.Assert.*;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.Test;
@@ -9,12 +10,17 @@ import org.junit.Test;
 public class AntDependenciesCollectorTest {
 
 	private AntDependenciesCollector tester = new AntDependenciesCollector(
-			"src/test/resources/firstlevel");
+			"src/test/resources/tk/jeromefromcn/firstlevel");
 
 	@Test
 	public void testCollectDependencies() {
 		Set<String> dependenciesSet = tester.collectDependencies();
-		assertEquals(dependenciesSet.size(), 2);
+		Set<String> compareSet = new HashSet<String>();
+		compareSet
+				.add("${M2_REPO}/spring3.0/server/org.springframework.web-3.0.3.RELEASE.jar");
+		compareSet
+				.add("${M2_REPO}/spring3.0/server/org.springframework.web.servlet-3.0.3.RELEASE.jar");
+		assertEquals(dependenciesSet, compareSet);
 	}
 
 }
